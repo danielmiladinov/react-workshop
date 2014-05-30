df2fa18810edfa4a9a97ce53ed46f48703b37365
Daniel Miladinov <dmiladinov@wingspan.com>
2014-05-30 10:32:47 -0400

    End state after phase

--- form-master-detail/webapp/js/Page.js
+++ form-master-detail/webapp/js/Page.js
@@ -11,15 +11,52 @@ define([
     var ContactModel = JSON.parse(ContactModel).data;
     var contacts = JSON.parse(contacts).data;
 
+    var FormField = Forms.FormField;
+    var AutoField = Forms.AutoField;
+    var AutoControl = Forms.AutoControl;
+
+    var AutoForm = React.createClass({
+        getDefaultProps: function () {
+            return {
+                metadata: {},
+                form: {},
+                onChange: function (fieldName, fieldValue) {}
+            }
+        },
+        render: function () {
+            var controls = _.map(this.props.metadata, function (fieldInfo) {
+                return (
+                    <AutoField
+                        fieldInfo={fieldInfo}
+                        value={this.props.form[fieldInfo.name]}
+                        onChange={_.partial(this.props.onChange, fieldInfo.name)} />
+                );
+            }.bind(this));
+            return (
+                <div>
+                    {controls}
+                </div>
+            );
+        }
+    });
 
     var App = React.createClass({
         getInitialState: function () {
             return {
-                MasterDetail: {
-                    database: contacts,
-                    form: contacts[0]
-                }
+                "firstName": '',
+                "lastName": '',
+                "phoneNumber": '',
+                "email": '',
+                "contactGroup": 'friend',
+                "id": "12345",
+                "revision": 1
             };
+        },
+
+        onChange: function (fieldName, fieldValue) {
+            var nextState = {};
+            nextState[fieldName] = fieldValue;
+            this.setState(nextState);
         },
 
         render: function () {
@@ -27,6 +64,10 @@ define([
 
             return (
                 <div className="App">
+                    <AutoForm
+                        metadata={ContactModel.properties}
+                        form={this.state}
+                        onChange={this.onChange} />
                     <pre>{JSON.stringify(this.state, undefined, 2)}</pre>
                 </div>
             );
